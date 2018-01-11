3f01a4101f79740efdd96cc5d02b3f0c6f673bbf
drklrd <srvbhattarai@gmail.com>
2018-01-11 19:38:50 +0545

    add icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Dateutil from 'ad-bs-converter';
 import Moment from 'moment';
-import { tada } from 'react-animations';
+import { pulse } from 'react-animations';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Radium, {StyleRoot} from 'radium';
@@ -10,9 +10,9 @@ import Nepalize from './lib';
 const images = 3;
 const imageIndex = Math.floor(Math.random() * images);
 const styles = {
-    tada: {
+    pulse: {
         animation: 'x 1s',
-        animationName: Radium.keyframes(tada, 'tada')
+        animationName: Radium.keyframes(pulse, 'pulse')
     }
 };
 
@@ -23,7 +23,7 @@ class App extends React.Component{
         const currentDate = Moment();
         this.state = {
             formattedDate : currentDate.format('YYYY/MM/DD'),
-            readableDate : currentDate.format('YYYY, MMMM DD'),
+            readableDate : currentDate.format('MMMM DD, YYYY '),
             currentTime : currentDate.format('hh:mm:a')
         }
     }
@@ -41,36 +41,32 @@ class App extends React.Component{
 
     renderTime(time){
         const timePartial  = Nepalize.nepalize(`${time.split(':')[0]}:${time.split(':')[1]}`);
+        const timeToReturn = {
+            time : timePartial
+        };
         if(time.split(':')[2]=='am'){
-            return `अहिले बिहानको ${timePartial} बजेको छ`;
+            timeToReturn['stage'] = 'बिहानको';
+            timeToReturn['icon'] = 'wi wi-sunrise';
         }else{
             if(time.split(':')[0]>6){
-                return `अहिले बेलुकाको ${timePartial} बजेको छ`;
+                timeToReturn['stage'] = 'बेलुकाको';
+                timeToReturn['icon'] = 'wi wi-moonrise';
             }else{
-                return `अहिले दिउसको ${timePartial} बजेको छ `;
+                timeToReturn['stage'] = 'दिउसको';
+                timeToReturn['icon'] = 'wi wi-day-sunny';
             }
         }
+        return timeToReturn;
     }
 
 	render(){
 	    const nepaliDateObj = Dateutil.ad2bs(this.state.formattedDate).ne;
-	    const nepaliDate = `${nepaliDateObj.year} साल, ${nepaliDateObj.strMonth} ${nepaliDateObj.day} गते `;
+	    const nepaliDate = `${nepaliDateObj.year} साल, ${nepaliDateObj.strMonth} ${nepaliDateObj.day} गते, ${nepaliDateObj.strDayOfWeek} `;
 		return(
 			<div>
                 <img src={`img/nepal${imageIndex}.jpg`} alt="Background Image" className="background-image"/>
                 <div className="contents">
-                    <div className="row">
-                        <div className="col-xs-offset-2 col-xs-8 text-center">
-                            <span className="time">
-                                <StyleRoot>
-                                    <div style={styles.tada}>
-                                        { this.renderTime(this.state.currentTime)}
-                                    </div>
-							    </StyleRoot>
-                            </span>
-                        </div>
-                    </div>
-                    <div className="row margin-date">
+                    <div className="row ">
                         <div className="col-xs-offset-2 col-xs-8 text-center">
                             <span className="nepali-date">
                                 { nepaliDate}
@@ -81,6 +77,24 @@ class App extends React.Component{
                         <div className="col-xs-offset-2 col-xs-8 text-center">
                             <span className="english-date">
                                 { this.state.readableDate }
+                            </span>
+                        </div>
+                    </div>
+                    <div className="row margin-date">
+                        <div className="col-xs-offset-2 col-xs-8 text-center">
+                            <span>
+                                <StyleRoot>
+                                    <div style={styles.pulse}>
+                                        <span className="stage">
+                                            <i className={this.renderTime(this.state.currentTime)['icon']}></i> &nbsp;
+                                            { this.renderTime(this.state.currentTime)['stage']}
+                                        </span>
+                                        <br/>
+                                        <span className="time">
+                                            { this.renderTime(this.state.currentTime)['time']}
+                                        </span>
+                                    </div>
+							    </StyleRoot>
                             </span>
                         </div>
                     </div>
