436d16bc04f9d2e457b2b0326d68013290813550
drklrd <srvbhattarai@gmail.com>
2018-01-25 19:44:29 +0545

    complete basic design for todo component

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const todos = [
+    {
+        todo : 'buy tc',
+        done : true
+    },{
+        todo : 'go to movie',
+        done : false
+    }
+
+];
+
 class Todo extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.taskDone = this.taskDone.bind(this);
+    }
+
+    taskDone(index){
+        alert(index);
+    }
+
+    renderToDos(){
+        const totosTemlate = todos.map((todo,index)=>{
+            return(
+                <div className="row todo col-xs-offset-4">
+                    <div className="col-xs-1">
+                        <span className={todo.done ? "fa fa-check-circle-o pointer" : "fa fa-circle-thin pointer"} onClick={()=>this.taskDone(index)}></span>
+                    </div>
+                    <div className={todo.done ? 'col-xs-7 align-left to-done' : 'col-xs-7 align-left'}>
+                        { todo.todo }
+                    </div>
+                </div>
+            );
+        });
+        return totosTemlate;
+    }
 
     render(){
         return(
             <div>
-                Todo
-                <input type="text" className="todo-input" placeholder="याहा लेकहनुहोस"/>
+                <h2>आज भोली गर्नु पर्ने केहि कार्यहरु यहाँ टिप्नुहोस </h2>
+
+                <div className="row">
+                    <input type="text" className="todo-input" placeholder="यहाँ लेकहनुहोस"/>
+                </div>
+
+                { !todos.length &&
+                    <h3>
+                        No todos added
+                    </h3>
+                }
+
+                {
+                    todos.length &&
+                        <div className="todos-list">
+                            { this.renderToDos() }
+                        </div>
+
+                }
+
             </div>
         );
     }
